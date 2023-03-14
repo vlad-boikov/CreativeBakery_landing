f122a3dc51c848211dfec653a34ad41593276c2c
Vlad Boikov <vladboikov95@hotmail.com>
2023-03-14 14:43:42 +0200

    Creative Bakery landing page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const socialNetworkLogos
+  = document.querySelectorAll('.social-networks__logo');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body_with-menu');
+  } else {
+    document.body.classList.remove('page__body_with-menu');
+  }
+});
+
+socialNetworkLogos.forEach((logo, ind) => {
+  if (ind !== socialNetworkLogos.length) {
+    logo.addEventListener('blur', () => {
+      socialNetworkLogos[ind + 1].focus();
+    });
+  }
+});
